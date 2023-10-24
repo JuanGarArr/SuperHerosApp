910803f8bb3caad147d18fb3b8a499823ec7b1f6
JuanGarArr <juan.gararr@gmail.com>
2023-10-24 16:52:54 +0200

    Creaccion de la estructura de carpetas con domain finalizado por el momento.

--- app/src/main/java/com/iesam/features/superHeros/data/local/XmlHeroDataSource.kt
+++ app/src/main/java/com/iesam/features/superHeros/data/local/XmlHeroDataSource.kt
@@ -1,4 +1,29 @@
 package com.iesam.features.superHeros.data.local
 
-class XmlHeroDataSource {
+import android.content.Context
+import com.google.gson.Gson
+import com.iesam.app.error.ErrorApp
+import com.iesam.app.functional.Either
+import com.iesam.app.functional.left
+import com.iesam.app.functional.right
+import com.iesam.features.superHeros.domain.models.Hero
+
+class XmlHeroDataSource (private val context: Context){
+
+    val sharedPref = context.getSharedPreferences("Heroes", Context.MODE_PRIVATE)
+
+    private val gson = Gson()
+
+    fun getHero():Either<ErrorApp, Hero>{
+        return try {
+            val jsonUsers = sharedPref.all as Map<String, String>
+            val hero = jsonUsers.values.map {
+                gson.fromJson(it, Hero::class.java)
+            }
+            return getHero()
+        }catch (ex:Exception){
+            return ErrorApp.UnknowError.left()
+        }
+    }
+
 }
